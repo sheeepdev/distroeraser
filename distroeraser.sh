9529a524cf3f4023f485391dbc61712969c524ad
sheep <sheep@sheepdev.xyz>
2022-08-01 19:34:58 +0200

    ✨ Update

--- distroeraser.sh
+++ distroeraser.sh
@@ -7,17 +7,12 @@ gum style \
 sleep 1
 echo "It is similar to other disk erasers but its much simpler because it uses dd"
 sleep 1
+echo "Go on and pick a disk you want to erase:"
+DISK=$(lsblk -d | tail -n+2 | awk '{print $1" "$4}' | gum choose --limit 1)
+echo "Ok, erasing disk $DISK"
 gum style \
 --foreground 210 --border normal --border-foreground 210 \
 --align center --width 50 --margin "1 2" --padding "2 4" \
 'IM NOT RESPONSIBLE FOR ANY UNWANTED ERASING. USE THIS SCRIPT AT YOUR OWN RISK'
 sleep 1
-gum confirm "Are you sure you want to permanently erase your system?" && echo "Erasing the drive in:" || echo "Cancelled"
-sleep 1
-echo "3..."
-sleep 1
-echo "2..."
-sleep 1
-echo "1..."
-sleep 1
-gum spin -s minidot --title 'Erasing the system...' dd if=/dev/zero of=/dev/sda bs=1M status=progress
+gum confirm "Are you sure you want to permanently erase your system?" && gum spin -s minidot --title 'Erasing the system...' dd if=/dev/zero of="$DISK" bs=1M status=progress || echo "Cancelled"
