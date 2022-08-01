c79fb2dd219b862bece2734b60b3538a4e4b8f79
sheep <sheep@sheepdev.xyz>
2022-08-01 18:56:40 +0200

    ✨ Now using gum

--- distroeraser.sh
+++ distroeraser.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-figlet "THE DISTROERASER"
-echo "This script erases your disk to get it ready for another OS/distro!"
+gum style \
+--foreground 110 --border normal --border-foreground 110 \
+--align center --width 50 --margin "1 2" --padding "2 4" \
+'THE DISTROERASER' 'is a script erases your disk to get it ready for another OS/distro!'
 sleep 1
 echo "It is similar to other disk erasers but its much simpler because it uses dd"
 sleep 1
-echo "IM NOT RESPONSIBLE FOR ANY UNWANTED ERASING. USE THIS SCRIPT AT YOUR OWN RISK"
+gum style \
+--foreground 210 --border normal --border-foreground 210 \
+--align center --width 50 --margin "1 2" --padding "2 4" \
+'IM NOT RESPONSIBLE FOR ANY UNWANTED ERASING. USE THIS SCRIPT AT YOUR OWN RISK'
 sleep 1
-read -p "Press any key to continue"
-echo "Erasing the drive in:"
+gum confirm "Are you sure you want to permanently erase your system?" && echo "Erasing the drive in:" || echo "Cancelled"
 sleep 1
 echo "3..."
 sleep 1
@@ -16,5 +20,4 @@ echo "2..."
 sleep 1
 echo "1..."
 sleep 1
-echo "Erasing the system..."
-dd if=/dev/zero of=/dev/sda bs=1M status=progress
+gum spin -s minidot --title 'Erasing the system...' dd if=/dev/zero of=/dev/sda bs=1M status=progress
